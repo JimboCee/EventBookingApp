d3ced4ebd0ed294304d1e246d8b3e813b8e50bec
JimboCee <49816802+JimboCee@users.noreply.github.com>
2022-04-11 16:18:08 +0100

    Added Navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Music',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sport',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Theatre',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign In',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
