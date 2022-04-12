3c5885d617eecec1ed44e7a48996238165c73979
Jimbo <jimmycranham@gmail.com>
2022-04-12 16:34:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar'
 import './App.css';
+import HeroSection from './components/HeroSection';
+
+
 
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <HeroSection />
     </div>     
   )
 }
-
-  
-
 export default App;
